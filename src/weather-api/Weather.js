f35ea643d2cddbe89d50c38e1ff17c251609e7df
Calvin8606 <calvin71203@gmail.com>
2023-06-21 14:09:53 -0400

    basic weather page setup

--- src/weather-api/Weather.js
+++ src/weather-api/Weather.js
@@ -1,5 +1,14 @@
+import "./Weather.css"
 function Weather() {
-  return <h1>HELLO TESTING</h1>;
+  return (
+    <>
+  <div class="header">
+    <h1>Mars Weather</h1>
+  </div>
+  
+  
+  </>
+  );
 }
 
 export default Weather;
