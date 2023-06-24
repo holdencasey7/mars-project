c1f5773af30c94fb37a6054c3e2875f8f446bf26
Holden Casey <me@holdencasey.com>
2023-06-24 00:33:03 -0400

    first attempt at image gallery

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./weather-api/Weather";
 import Image from "./image-api/Image";
+import Gallery from "./image-api/Gallery";
+import { useState } from "react";
 
 function App() {
+  const [sol, setSol] = useState(0);
+
   return (
     <>
-      <Image />
+      <form>
+        <label>
+          Enter Sol:
+          <input
+            type="number"
+            value={sol}
+            onChange={(e) => setSol(e.target.value)}
+          />
+        </label>
+      </form>
+      <Gallery sol={sol} />
     </>
   );
 }
